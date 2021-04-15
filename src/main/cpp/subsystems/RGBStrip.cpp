6fd245b0830376ca1f5bd41c751add3438e1e445
DJAY101 <55177986+DJAY101@users.noreply.github.com>
2021-04-16 09:19:07 +1000

    Update RGBStrip.cpp

--- src/main/cpp/subsystems/RGBStrip.cpp
+++ src/main/cpp/subsystems/RGBStrip.cpp
@@ -23,6 +23,7 @@ void RGBStrip::SetRGBStrip(double R, double G, double B) {
 }
 
 void RGBStrip::Rainbow() {
+    
     srand (time(NULL));
     double randomR = (rand() % 9 + 1);
     srand (time(NULL)-20);
@@ -34,19 +35,20 @@ void RGBStrip::Rainbow() {
     randomR = randomR/1000;
     randomR = randomR/1000;
     
-     if (rand() %3 == 1 && m_countR > rand() %10000 + 10000) {
+    if (rand() %3 == 1 && m_countR > rand() %10000 + 10000) {
 
         m_countR = 0;
         if (m_signR == -1) {
             m_signR = 1;
         } else {m_signR = -1;}
-     } else {m_countR++;}
-     if(m_R + (randomR*sign) > 255) {
+
+    } else {m_countR++;}
+    if(m_R + (randomR*m_signR) > 255) {
         m_signR = -1;
-     } else if (m_R + (randomR*m_signR) < 0) {
+    } else if (m_R + (randomR*m_signR) < 0) {
         m_signR = +1;
-       }
-            m_R = m_R + (randomR*m_signR);
+        }
+    m_R = m_R + (randomR*m_signR);
 
 
 //     if (m_R + randomR > 255) {
@@ -71,11 +73,11 @@ void RGBStrip::Rainbow() {
 
 
     m_R = m_R - randomR;
-    m_G = m_G - randomG;
-    m_B = m_B - randomB;
+    // m_G = m_G - randomG;
+    // m_B = m_B - randomB;
 
     m_strip->SetLEDOutput(m_R/255.0, CANifier::LEDChannelB);
-    m_strip->SetLEDOutput(m_G/255.0, CANifier::LEDChannelA);
-    m_strip->SetLEDOutput(m_B/255.0, CANifier::LEDChannelC);
+    // m_strip->SetLEDOutput(m_G/255.0, CANifier::LEDChannelA);
+    // m_strip->SetLEDOutput(m_B/255.0, CANifier::LEDChannelC);
 
 }
